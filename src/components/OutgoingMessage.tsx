b3d0c48b16315c188abf0cf92b86b1098ad69a8b
Johannes Andersson <hello@thejoltjoker.com>
2024-02-06 23:11:18 +0000

    Add zero padding

--- src/components/OutgoingMessage.tsx
+++ src/components/OutgoingMessage.tsx
@@ -4,9 +4,10 @@ type OutgoingMessageProps = {
 };
 
 const OutgoingMessage = ({ text, time }: OutgoingMessageProps) => {
+  const zeroPad = (n: number, pad = 2) => String(n).padStart(pad, "0");
   return (
     <div className="ml-auto w-fit">
-      <p className="mb-1 text-end text-sm text-neutral-500">{`${time.getHours()}:${time.getMinutes()}`}</p>
+      <p className="mb-1 text-end text-sm text-neutral-500">{`${zeroPad(time.getHours())}:${zeroPad(time.getMinutes())}`}</p>
 
       <div className="rounded-md bg-[#e9ebf9] p-4">
         <p>{text}</p>
